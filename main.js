4f24c739490d329603cd9924b1ea0b36a0ee5535
Rovshan Alizada <rovshanalizada@gamil.com>
2024-08-29 17:32:10 +0400

    done

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function ToDoLists() {
         <li>
                 <span>${item}</span> 
                 <div class="icons">
-                    <i class="fa-solid fa-pen" onclick ="edit()" ></i>
+                    <i class="fa-solid fa-pen" onclick="(getModal('${item}',${index}))" ></i>
                     <i style="margin-left: 1rem;" class="fa-solid fa-trash" onclick = "trash(${index})" ></i>
                 </div>
          </li>
@@ -115,108 +115,37 @@ li.forEach(item => {
 
 
 
+// edit the list items
 
 
 
+let modal = document.getElementById("modal-special")
 
+function getModal(item, index) {
+    console.log(item);
+    modal.innerHTML = ""
+    modal.innerHTML = `<i id="closeDetail" onclick="closedFunc()" class="fa-solid fa-x"></i>
+     <input id="valNew" value="${item}" type="text">
+     <button onclick="updatedFunc('${index}')">Save</button>`
+    modal.style.display = "flex"
+}
+function closedFunc() {
+    modal.style.display = "none"
+}
 
+function updatedFunc(index) {
+    let newInputValue = document.getElementById("valNew").value
+    let todo = JSON.parse(localStorage.getItem("todo")) || []
+    console.log(newInputValue);
+    if (!newInputValue.trim()) {
+        alert("zehmet olmasa yeni deyeri daxil edin")
+    } else {
+        todo.splice(index, 1, `${newInputValue}`)
+        // todo.forEach((item, i) => i == index ? todo.splice(i, 1, newInputValue) : null)
+        console.log(todo);
+    }
+    localStorage.setItem("todo", JSON.stringify(todo))
+    ToDoLists()
+    closedFunc()
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// let data = JSON.parse(localStorage.getItem("todolist")) || []; 
-
-// addToDo(data);
-
-// toDoForm.addEventListener("submit", getForm);
-
-// function getForm(e) {
-//     e.preventDefault();
-//     data.push(toDoInput.value);
-//     toDoInput.value = '';
-//     saveData();
-// }
-
-// function addToDo(data) {
-//     list.innerHTML = "";
-//     data.map((item, index) => {
-//         list.innerHTML += `
-//          <li>
-//                 <span>${item}</span> 
-//                 <div class="icons">
-//                     <i class="fa-solid fa-pen"></i>
-//                     <i style="margin-left: 1rem;" class="fa-solid fa-trash" onclick="trash(${index})"></i>
-//                 </div>
-//         </li>
-//         `
-//     });
-
-//     // Add click event listener to each list item
-//     const listItems = document.querySelectorAll("li");
-//     listItems.forEach(li => {
-//         li.addEventListener("click", (e) => {
-//             e.target.style.backgroundColor = "green";
-//             e.target.style.color = "white";
-//             e.target.style.textDecoration = "line-through";
-//         });
-//     });
-// }
-
-// // Function to save data to localStorage and update the list
-// function saveData() {
-//     localStorage.setItem("todolist", JSON.stringify(data));
-//     addToDo(data); 
-// }
-
-// // Clear all lists
-// document.querySelector(".clearAll").addEventListener("click", totalTrash);
-// function totalTrash() {
-//     data = [];
-//     saveData();
-// }
-
-// // Delete each element
-// function trash(index) {
-//     data.splice(index, 1);
-//     saveData();
-// }
-
-// // Search elements
-// document.querySelector("#search").addEventListener("input", search);
-// function search(e) {
-//     const searchTerm = e.target.value.toLowerCase();
-//     const filteredData = data.filter(item => item.toLowerCase().includes(searchTerm));
-//     addToDo(filteredData);
-// }
